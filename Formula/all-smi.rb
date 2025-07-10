59f72c1ee4d8de3bf3dcc0b84aacf51acfba0d18
Jeongkyu Shin <inureyes@gmail.com>
2025-07-11 02:30:57 +0900

    update: all-smi to v0.4.3

--- Formula/all-smi.rb
+++ Formula/all-smi.rb
@@ -1,19 +1,19 @@
 class AllSmi < Formula
   desc      "GPU ‘top’ for NVIDIA/Apple Silicon/Jetson"
   homepage  "https://github.com/inureyes/all-smi"
-  version   "0.4.2"
+  version   "0.4.3"
   license   "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url     "https://github.com/inureyes/all-smi/releases/download/v0.4.2/all-smi-macos-aarch64.zip"
-      sha256  "eb1eda8fccf88990bedc7ea7de4eb464770f38e70119a60e9d167d635e3becca"
+      url     "https://github.com/inureyes/all-smi/releases/download/v0.4.3/all-smi-macos-aarch64.zip"
+      sha256  "48529c5e0c591149897845f6b50a26041575a5bbd7f8407c7b5b2d82221da174"
     end
   end
 
   on_linux do
-    url      "https://github.com/inureyes/all-smi/releases/download/v0.4.2/all-smi-linux-x86_64.tar.gz"
-    sha256   "9f4a2a8f365b452e38fc3b33907d0db9bd8ef27ce62c90178de13471284e7f5d"
+    url      "https://github.com/inureyes/all-smi/releases/download/v0.4.3/all-smi-linux-x86_64.tar.gz"
+    sha256   "2823959fc9a59bcf0c0c9b0a98bc7789cd86b2c49c3e299dd748261ecd6e2300"
   end
 
   def install
