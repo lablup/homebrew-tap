8889cf211f1c3f0637e73e01e7230b9fd24773a5
GitHub Action <actions@github.com>
2025-07-26 05:19:09 +0000

    bump: all-smi to v0.6.2

--- Formula/all-smi.rb
+++ Formula/all-smi.rb
@@ -8,6 +8,7 @@ class AllSmi < Formula
     if Hardware::CPU.arm?
       url     "https://github.com/inureyes/all-smi/releases/download/v0.6.2/all-smi-macos-aarch64.zip"
       sha256  "4ee12a4c53ba01da5d24ebe8f68136babcaec5441fa988c59abdcd1b803dd091"
+      sha256  "4ee12a4c53ba01da5d24ebe8f68136babcaec5441fa988c59abdcd1b803dd091"
     end
   end
 
@@ -15,9 +16,11 @@ class AllSmi < Formula
     if Hardware::CPU.arm?
       url      "https://github.com/inureyes/all-smi/releases/download/v0.6.2/all-smi-linux-aarch64.tar.gz"
       sha256   "d0b7325045cfdf17fd44a68d3b8477cb3a9a2c74301f0aa9f514e03b90097af9"
+      sha256   "d0b7325045cfdf17fd44a68d3b8477cb3a9a2c74301f0aa9f514e03b90097af9"
     end
     if Hardware::CPU.intel?
       url      "https://github.com/inureyes/all-smi/releases/download/v0.6.2/all-smi-linux-x86_64.tar.gz"
+      sha256   "3f9426dc7f44dd1609b4bd5955b7fa5f73607a8f40df6c7a400b036c629031f8"
       sha256   "3f9426dc7f44dd1609b4bd5955b7fa5f73607a8f40df6c7a400b036c629031f8"
     end
   end
