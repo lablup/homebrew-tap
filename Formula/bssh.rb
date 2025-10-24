0edda768a1cb7dfb718df01a545b3828f44f8781
GitHub Action <actions@github.com>
2025-10-24 01:56:16 +0000

    bump: bssh to v1.0.0

--- Formula/bssh.rb
+++ Formula/bssh.rb
@@ -7,18 +7,18 @@ class Bssh < Formula
   on_macos do
     if Hardware::CPU.arm?
       url     "https://github.com/lablup/bssh/releases/download/v1.0.0/bssh-macos-aarch64.zip"
-      sha256 "d104ee6dc87e2313d22da2bb736a62fc345a685c5bca563934891b66e123a988"
+      sha256 "d723a8e7d3c96361b86570e4a4c528c4bc69e44909066b5cdf950ad9c5680e69"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url      "https://github.com/lablup/bssh/releases/download/v1.0.0/bssh-linux-aarch64.tar.gz"
-      sha256 "565d7dbf070b799266c7805970e2c471e26fa19c08563a8efadaf760a8587eb0"
+      sha256 "2e86e6d1f16bbd09ebe6d40be28dbea71a34a7d2e22a2ae0904c7295f85c38ea"
     end
     if Hardware::CPU.intel?
       url      "https://github.com/lablup/bssh/releases/download/v1.0.0/bssh-linux-x86_64.tar.gz"
-      sha256 "92f885e9a01b2cc8bef2f1c397d122b4962584eee7212afc87399752c9488a17"
+      sha256 "e0a280d5cffcaeadc378437fcb45f2d5f2360e3d2ca1cb7ef5fad3676366adec"
     end
   end
 
