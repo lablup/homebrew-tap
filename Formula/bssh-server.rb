14d26c2cdf67b7670a6cc68e16fe55c1da02877f
Jeongkyu Shin <inureyes@gmail.com>
2026-01-29 12:50:18 +0900

    feat: Add bssh-server formula

---
+++ Formula/bssh-server.rb
@@ -0,0 +1,33 @@
+class BsshServer < Formula
+  desc      "Lightweight SSH server for containers (part of bssh project)"
+  homepage  "https://github.com/lablup/bssh"
+  version "1.7.0"
+  license   "Apache-2.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url     "https://github.com/lablup/bssh/releases/download/v1.7.0/bssh-server-macos-aarch64.zip"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url      "https://github.com/lablup/bssh/releases/download/v1.7.0/bssh-server-linux-aarch64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    if Hardware::CPU.intel?
+      url      "https://github.com/lablup/bssh/releases/download/v1.7.0/bssh-server-linux-x86_64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "bssh-server"
+  end
+
+  test do
+    output = shell_output("#{bin}/bssh-server --version")
+    assert_match(/bssh-server\s+#{version}/, output)
+  end
+end
