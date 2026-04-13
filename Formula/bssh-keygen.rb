0853b4a8def058c32fc69bc46ce111660dbf9547
Jeongkyu Shin <jeongkyu.shin@gmail.com>
2026-04-13 23:17:28 +0900

    feat: Add bssh-keygen formula

---
+++ Formula/bssh-keygen.rb
@@ -0,0 +1,34 @@
+class BsshKeygen < Formula
+  desc      "SSH key generator utility for bssh (part of bssh project)"
+  homepage  "https://github.com/lablup/bssh"
+  version "2.0.0"
+  license   "Apache-2.0"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url     "https://github.com/lablup/bssh/releases/download/v2.0.0/bssh-keygen-macos-aarch64.zip"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url      "https://github.com/lablup/bssh/releases/download/v2.0.0/bssh-keygen-linux-aarch64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+    if Hardware::CPU.intel?
+      url      "https://github.com/lablup/bssh/releases/download/v2.0.0/bssh-keygen-linux-x86_64.tar.gz"
+      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    end
+  end
+
+  def install
+    bin.install "bssh-keygen"
+    man1.install "bssh-keygen.1"
+  end
+
+  test do
+    output = shell_output("#{bin}/bssh-keygen --version")
+    assert_match(/bssh-keygen\s+#{version}/, output)
+  end
+end
