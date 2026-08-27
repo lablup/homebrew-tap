fdd12a2564f741a0ee1c41a0aa32fe5d6f478974
GitHub Action <actions@github.com>
2026-08-27 01:07:26 +0000

    bump: all-smi to v0.26.2

--- Formula/all-smi.rb
+++ Formula/all-smi.rb
@@ -1,33 +1,34 @@
 class AllSmi < Formula
   desc      "GPU ‘top’ for NVIDIA/Jetson/Apple Silicon/Tenstorrent"
   homepage  "https://github.com/lablup/all-smi"
-  version "0.26.1"
+  version "0.26.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/lablup/all-smi/releases/download/v0.26.1/all-smi-macos-aarch64.zip"
-      sha256 "5f9ec117ece34eef2277fe9d8da98fc263fa82dfe818031d2c6540775494afd9"
+      url "https://github.com/lablup/all-smi/releases/download/v0.26.2/all-smi-macos-aarch64.zip"
+      sha256 "bb934940a198a96130c558a7829009476954d9117389a8bf7cb9ffa40094eb3a"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/lablup/all-smi/releases/download/v0.26.1/all-smi-macos-x86_64.zip"
-      sha256 "70f9202df2bffcd0b398c904418a516cb252fecbf88ccb0a66bc37bc0ea5a3bc"
+      url "https://github.com/lablup/all-smi/releases/download/v0.26.2/all-smi-macos-x86_64.zip"
+      sha256 "a2bdc70abae18e5a48b9fa87eed6ba09ed4b45baaf30f13baa48434417ab5503"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/lablup/all-smi/releases/download/v0.26.1/all-smi-linux-aarch64.tar.gz"
-      sha256 "9c186f666664ea5925385c7fbcbb2448ec90869786e2f85df0d3b3b2437eecb2"
+      url "https://github.com/lablup/all-smi/releases/download/v0.26.2/all-smi-linux-aarch64.tar.gz"
+      sha256 "4ab023cf21927b255a4caf12c6149e24ae2782c994b07725891996c8b0e6ce18"
     end
     if Hardware::CPU.intel?
-      url "https://github.com/lablup/all-smi/releases/download/v0.26.1/all-smi-linux-x86_64.tar.gz"
-      sha256 "3c49f9ed17db31d5fa5d4962b75e177deb9fafcb708dc5e38041f073ccc00cbe"
+      url "https://github.com/lablup/all-smi/releases/download/v0.26.2/all-smi-linux-x86_64.tar.gz"
+      sha256 "846af2669cfee5ef1b6c0d48e52ed882d0598757c3a647acecd18415ef7975d6"
     end
   end
 
   def install
     bin.install "all-smi"
+    (lib/"all-smi").install "liball_smi_amd.so" if OS.linux?
     man1.install "all-smi.1"
   end
 
